44a4005b1c76d82382188a03ef68cbe44f8661bb
Deepak-96adt <deepakupadhyay.adt@gmail.com>
2024-12-12 17:00:07 +0530

    index page

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'semantic-ui-css/semantic.min.css'
 
+// this is index file
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
